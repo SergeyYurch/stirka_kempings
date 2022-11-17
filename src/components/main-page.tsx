86022a8f673ec55764d184ac0be35b87bc9986bb
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-17 21:04:42 +0200

    added firs component

---
+++ src/components/main-page.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+export default function MainPage() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                Header
+
+            </Container>
+        </React.Fragment>
+    );
+}
